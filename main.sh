30a135ec89b0469c03a2e73ad9a125cb132a2e39
ToastXC <100072983+toastxc@users.noreply.github.com>
2022-08-13 22:57:42 +0800

    Added checks for already installed certificates

--- main.sh
+++ main.sh
@@ -1,25 +1,41 @@
+
 echo "Shenton Cert Kit"
 echo "If you are not attending Shenton College DO NOT RUN THIS SCRIPT"
 
 mkdir certs
-mkdir certs/curled
-mkdir certs/system
-mkdir certs/firefox
-
-#wget -b https://certs.education.wa.edu.au/education-pki/cert/Education-CA.cer
-#wget -b https://certs.education.wa.edu.au/education-pki/cert/Education-SubCA1.cer
-#wget -b https://certs.education.wa.edu.au/education-pki/cert/Education-SubCA2.cer
-
-curl --create-dirs -O --output-dir certs https://certs.education.wa.edu.au/education-pki/cert/Education-CA.cer
-curl -O --output-dir https://certs.education.wa.edu.au/education-pki/cert/Education-SubCA1.cer
-curl -O --output-dir  https://certs.education.wa.edu.au/education-pki/cert/Education-SubCA2.cer
-
 mkdir system-cert
 
-openssl x509 -in certs/Education-CA.cer -out system-cert/Education-CA.pem
-openssl x509 -in certs/Education-SubCA2.cer -out system-cert/Education-SubCA2.pem
-openssl x509 -in certs/Education-SubCA1.cer -out system-cert/Education-SubCA1.pem
+CA=$(ls /etc/ssl/certs | grep Education-CA);
+SubCA1=$(ls /etc/ssl/certs | grep Education-SubCA1);
+SubCA2=$(ls /etc/ssl/certs | grep Education-SubCA2);
 
-sudo cp system-cert/Education-CA.pem /etc/certs/
-sudo cp system-cert/Education-SubCA1.pem /etc/certs/
-sudo cp system-cert/Education-SubCA2.pem /etc/certs/
+# debug
+echo $CA
+echo $SubCA1
+echo $SubCA2
+
+
+if [[ $CA = "Education-CA.pem" ]]; then 
+	echo "CA cert found"
+else
+	curl -O --output-dir certs https://certs.education.wa.edu.au/education-pki/cert/Education-CA.cer
+	openssl x509 -in certs/Education-CA.cer -out system-cert/Education-CA.pem
+	sudo cp system-cert/Education-CA.pem /etc/certs/
+fi
+
+if [[ $SubCA1 = "Education-SubCA1.pem" ]]; then  
+        echo "SubCA1 cert found"
+else
+	curl -O --output-dir https://certs.education.wa.edu.au/education-pki/cert/Education-SubCA1.cer
+	openssl x509 -in certs/Education-SubCA1.cer -out system-cert/Education-SubCA1.pem
+	sudo cp system-cert/Education-SubCA1.pem /etc/certs/
+	
+fi
+
+if [[ $SubCA2 = "Education-SubCA2.pem" ]]; then
+        echo "SubCA2 cert found"
+else
+	curl -O --output-dir  https://certs.education.wa.edu.au/education-pki/cert/Education-SubCA2.cer
+	openssl x509 -in certs/Education-SubCA2.cer -out system-cert/Education-SubCA2.pem
+	sudo cp system-cert/Education-SubCA2.pem /etc/certs/
+fi
