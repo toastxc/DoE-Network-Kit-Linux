83d706bb0fba80e90b1772db790f7797f2263d9d
ToastXC <100072983+toastxc@users.noreply.github.com>
2022-08-22 18:50:29 +0800

    Update and rename temp.sh to main.sh

--- main.sh
+++ main.sh
@@ -5,20 +5,10 @@ if (( $EUID != 0 )); then
         exit
 fi
 
-wifiadapt=$(ls /sys/class/net  | grep w)
-
-if [[ $wifiadapt = "" ]]; then
-	echo "no wifi compatible devie found"
-	exit
-fi
-
-if (($OSTYPE != "linux-gnu")); then
-	echo "this script is for GNU/Linux only:"
-	exit
-fi
 
 ############################################### WIFI SETTINGS ######################################################
 
+echo "WIFI"
 
 # wifi section 
 
@@ -55,10 +45,10 @@ if [[ $usernamecheck = "" ]]; then
 	echo "invalid username, try firstname.lastname"
 fi
 
-if [[ $autocon = "yes" ]]; then
-	autoconnect=$(echo "true")
-else
+if [[ $autocon = "no" ]]; then
 	autoconnect=$(echo "false")
+else
+	autoconnect=$(echo "true")
 fi
 
 
@@ -66,55 +56,42 @@ fi
 touch /etc/NetworkManager/system-connections/WIRELESS-2.4.nmconnection
 
 parse=$(cat <<EOF
-[connection]\n
-id=WIRELESS-2.4\n
-uuid=ac9c73e7-783d-46f9-a10a-936807a87d08\n
-type=wifi\n
-autoconnect=$autoconnect\n
-interface-name=$wifiadapt\n
-\n
-[wifi]\n
-mode=infrastructure\n
-ssid=WIRELESS-2.4\n
-\n
-[wifi-security]\n
-key-mgmt=wpa-eap\n
-\n
-[802-1x]\n
-eap=peap;\n
-identity=$username\n
-password=$password\n
-phase2-auth=mschapv2\n
-\n
-[ipv4]\n
-method=auto\n
-\n
-[ipv6]\n
-addr-gen-mode=stable-privacy\n
-method=auto\n
-\n
-[proxy]\n
+[connection]\nid=WIRELESS-2.4\nuuid=ac9c73e7-783d-46f9-a10a-936807a87d08\ntype=wifi\nautoconnect=$autoconnect\ninterface-name=wlp8s0\n\n[wifi]\nmode=infrastructure\nssid=WIRELESS-2.4\n\n[wifi-security]\nkey-mgmt=wpa-eap\n\n[802-1x]\neap=peap\n$identity$username\npassword=$password\nphase2-auth=mschapv2\n\n[ipv4]\nmethod=auto\n\n[ipv6]\naddr-gen-mode=stable-privacy\nmethod=auto\n\n[proxy]
 EOF
 )
 
+
 # disable for safety
+
+touch login.info
+echo -e $parse > login.info
 echo -e $parse > /etc/NetworkManager/system-connections/WIRELESS-2.4.nmconnection
 
-systemctl restart NetworkManager
+nmcli connection reload
+
+sleep 1
+echo "connecting to wifi"
+nmcli connection up 'WIRELESS-2.4'
+sleep 1
+
+
 ######################################### CERTIFICATE INSTALLATION #################################################
 
+echo "CERTIFICATES"
 
 
 Certdir=$(ls | grep certs);
-Syscertdir=$(ls | grep system-cert);
 
 if [[ $Certdir != "certs" ]]; then
 	mkdir certs
+	mkdir certs/imp
+	touch certs/imp/Education-CA.cer
+	touch certs/imp/Education-SubCA1.cer
+	touch certs/imp/Education-SubCA2.cer
+
+	mkdir certs/system-cert
 fi
 
-if [[ $Syscertdir != "system-cert" ]]; then
-	mkdir system-cert
-fi
 
 
 CA=$(ls /etc/ssl/certs | grep Education-CA);
@@ -122,9 +99,12 @@ SubCA1=$(ls /etc/ssl/certs | grep Education-SubCA1);
 SubCA2=$(ls /etc/ssl/certs | grep Education-SubCA2);
 
 
-Shencheck=$(curl -s enrol.shenton.wa.edu.au);
+<< 'MULTILINE-COMMENT'
+
+Shencheck=$(curl -S https://enrol.shenton.wa.edu.au);
 
 echo $Shencheck
+
 
 if [[ $Shencheck = "" ]]; then
         echo "Could not reach Shenton repository, please try connecting to Shenton WiFi"
@@ -132,35 +112,47 @@ if [[ $Shencheck = "" ]]; then
 fi
 
 
+MULTILINE-COMMENT
+
 if [[ $CA = "Education-CA.pem" ]]; then
 	echo "CA cert found"
 else
-	curl -O --output-dir certs https://certs.education.wa.edu.au/education-pki/cert/Education-CA.cer
-	openssl x509 -in certs/Education-CA.cer -out system-cert/Education-CA.pem
-	cp system-cert/Education-CA.pem /etc/certs/
+	
+	echo "installing Edu-CA"
+	curl https://certs.education.wa.edu.au/education-pki/cert/Education-CA.cer > certs/imp/Education-CA.cer
+	openssl x509 -in certs/imp/Education-CA.cer -out certs/system-cert/Education-CA.pem
+	cp certs/system-cert/Education-CA.pem /etc/ssl/certs/
 fi
 
 if [[ $SubCA1 = "Education-SubCA1.pem" ]]; then
         echo "SubCA1 cert found"
 else
-	curl -O --output-dir https://certs.education.wa.edu.au/education-pki/cert/Education-SubCA1.cer
-	openssl x509 -in certs/Education-SubCA1.cer -out system-cert/Education-SubCA1.pem
-	cp system-cert/Education-SubCA1.pem /etc/certs/
+	echo "installing Edu-Sub-CA1"
+	curl https://certs.education.wa.edu.au/education-pki/cert/Education-SubCA1.cer > certs/imp/Education-SubCA1.cer
+	openssl x509 -in certs/system-cert/Education-SubCA1.cer -out system-cert/Education-SubCA1.pem
+	cp certs/system-cert/Education-SubCA1.pem /etc/ssl/certs/
 
 fi
 
 if [[ $SubCA2 = "Education-SubCA2.pem" ]]; then
         echo "SubCA2 cert found"
 else
-	curl -O --output-dir  https://certs.education.wa.edu.au/education-pki/cert/Education-SubCA2.cer
-	openssl x509 -in certs/Education-SubCA2.cer -out system-cert/Education-SubCA2.pem
-	cp system-cert/Education-SubCA2.pem /etc/certs/
+	
+	echo "installing Edu-SubCA2"
+	curl https://certs.education.wa.edu.au/education-pki/cert/Education-SubCA2.cer > certs/imp/Education-SubCA1.cer
+	openssl x509 -in certs/imp/Education-SubCA1.cer -out certs/system-cert/Education-SubCA2.pem
+	cp certs/system-cert/Education-SubCA2.pem /etc/ssl/certs/
 fi
-
 
 systemctl restart NetworkManager
 
 ########################################### FEDORA NETWORKING #####################################################
+echo "FEDORA"
+
+if (($OSTYPE != "linux-gnu")); then
+	echo "this script is for GNU/Linux only:"
+	exit
+fi
 
 DNF=$(ls /etc/ | grep dnf);
 
