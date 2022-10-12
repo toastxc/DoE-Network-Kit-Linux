c677cb3ee5a25889d70135e4dd2d6819a5938a4e
Declan Chidlow <84255570+DeclanChidlow@users.noreply.github.com>
2022-10-12 01:05:14 +0000

    Update spelling

--- shenton.sh
+++ shenton.sh
@@ -1,28 +1,28 @@
 ################################################## PREREQ ##########################################################
 if (($OSTYPE != "linux-gnu")); then
-        echo "this script is for GNU/Linux only:"
+        echo "This script is for GNU/Linux only."
         exit
 fi
 
 # sudo checker
 if (( $EUID != 0 )); then
-        echo "please run as root"
+        echo "Please run as root."
         exit
 fi
 
 
 ############################################### WIFI SETTINGS ######################################################
 
-echo "WIFI"
+echo "WI-FI"
 
 # credentials 
-echo "username: "
+echo "Username: "
 read username 
 
-echo "password: "
+echo "Password: "
 read password
 
-echo 'autoconnect: ("yes" or "no")'
+echo 'Autoconnect: ("yes" or "no")'
 read autocon
 
 
@@ -30,7 +30,7 @@ read autocon
 usernamecheck=$( echo $username | grep  \\. )
 
 if [[ $usernamecheck = "" ]]; then
-	echo "invalid username, try firstname.lastname"
+	echo "Username invalid, try 'firstname.lastname'."
 else
 	username=$( echo 'BLUE\'$username)
 fi
@@ -89,16 +89,16 @@ Shencheck=$(curl -S https://enrol.shenton.wa.edu.au);
 echo $Shencheck
 
 if [[ $Shencheck = "" ]]; then
-        echo "Could not reach Shenton repository, please try connecting to Shenton WiFi"
+        echo "Could not reach the Shenton repository. Please try connecting to Shenton WiFi."
 	exit
 fi
 
 
 if [[ $CA = "Education-CA.pem" ]]; then
-	echo "CA cert found"
+	echo "Education-CA cert found locally."
 else
 	
-	echo "installing Edu-CA"
+	echo "Installing Education-CA..."
 	touch certs/imp/Education-CA.cer
 	curl https://certs.education.wa.edu.au/education-pki/cert/Education-CA.cer > certs/imp/Education-CA.cer
 	openssl x509 -in certs/imp/Education-CA.cer -out certs/system-cert/Education-CA.pem
@@ -109,9 +109,9 @@ fi
 
 
 if [[ $SubCA1 = "Education-SubCA1.pem" ]]; then
-        echo "SubCA1 cert found"
+        echo "Education-SubCA1 cert found locally."
 else
-	echo "installing Edu-Sub-CA1"
+	echo "Installing Education-SubCA1..."
 	touch certs/imp/Education-SubCA1.cer
 	curl https://certs.education.wa.edu.au/education-pki/cert/Education-SubCA1.cer > certs/imp/Education-SubCA1.cer
 	openssl x509 -in certs/imp/Education-SubCA1.cer -out certs/system-cert/Education-SubCA1.pem
@@ -122,10 +122,10 @@ fi
 
 
 if [[ $SubCA2 = "Education-SubCA2.pem" ]]; then
-        echo "SubCA2 cert found"
+        echo "Education-SubCA2 cert found locally."
 else
 	
-	echo "installing Edu-SubCA2"
+	echo "Installing Education-SubCA2..."
 	touch certs/imp/Education-SubCA2.cer
 	curl https://certs.education.wa.edu.au/education-pki/cert/Education-SubCA2.cer > certs/imp/Education-SubCA1.cer
 	openssl x509 -in certs/imp/Education-SubCA1.cer -out certs/system-cert/Education-SubCA2.pem
